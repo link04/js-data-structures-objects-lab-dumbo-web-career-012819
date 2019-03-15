72722eaca16d05747a76e08031d0cdcee046ec7c
Maximo Bautista <max.bautista97@gmail.com>
2019-03-15 00:02:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const driver  = {};
 
 const updateDriverWithKeyAndValue = (driver, key, value) =>{
   const newObj = Object.assign({}, driver);
-  return newObj[key] = value;
+  newObj[key] = value;
+  return
 }
