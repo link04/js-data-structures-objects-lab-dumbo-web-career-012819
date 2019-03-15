0633e48ce78bee4eb0579e154e0aa05b27495f51
Maximo Bautista <max.bautista97@gmail.com>
2019-03-15 00:07:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const updateDriverWithKeyAndValue = (driver, key, value) =>{
 }
 
 const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) =>{
-  const newObj = Object.assign({}, driver);
-  delete newObj[key];
-  return newObj;
-
+  driver[key] = value;
+  return driver;
 }
 
 const deleteFromDriverByKey = (driver, key) =>{
